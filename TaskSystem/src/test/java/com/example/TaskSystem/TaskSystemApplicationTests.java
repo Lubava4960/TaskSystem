a22a4316266f720d1f01a195db8e10a8544f06d3
Lubov Martyanova <lmartyanova@hse.ru>
2025-09-01 11:34:29 +0300

    ашÐfirst commit

---
+++ TaskSystem/src/test/java/com/example/TaskSystem/TaskSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
